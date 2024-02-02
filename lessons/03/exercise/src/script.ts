7e5b819bcdeb3a512af0e2aa66b823ee021be9ec
Esa <esa@venngage.com>
2024-02-02 11:18:24 -0500

    lesson 9
    wrong number for thelast commits

--- lessons/03/exercise/src/script.ts
+++ lessons/03/exercise/src/script.ts
@@ -1,7 +1,18 @@
 import * as three from 'three';
 import './styles.css';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GUI from 'lil-gui';
+import gsap from 'gsap';
 
+/**
+ * Debug
+ */
+const gui = new GUI({
+    width: 300,
+    title: 'Esa\'s Debug UI',
+    closeFolders: false,
+});
+gui.hide();
 // cursor
 const cursor = {
 	x: 0,
@@ -12,6 +23,17 @@ const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight,
 };
+
+const debugObject: any = {
+    color: '#d67aab',
+    subdivions: 3,
+};
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'h') {
+        gui.show(gui._hidden);
+    }
+});
 
 window.addEventListener('mousemove', (event) => {
 	cursor.x = event.clientX / sizes.width - 0.5;
@@ -52,28 +74,52 @@ const canvas: any = document.querySelector('.webgl');
 const scene: three.Scene = new three.Scene();
 
 // object
-// const geometry: three.BoxGeometry = new three.BoxGeometry(1, 1, 1, 5, 5, 5);
-const geometry: three.BufferGeometry = new three.BufferGeometry();
-const count = 50;
-const positionsArray = new Float32Array(count * 3 * 3);
-for (let i = 0; i < count * 3 * 3; i++) {
-    positionsArray[i] = (Math.random() - 0.5) * 4;
-}
-const positionsAttribute = new three.BufferAttribute(positionsArray, 3);
-geometry.setAttribute('position', positionsAttribute);
+const geometry: three.BoxGeometry = new three.BoxGeometry(1, 1, 1, debugObject.subdivions, debugObject.subdivions, debugObject.subdivions);
+// const geometry: three.BufferGeometry = new three.BufferGeometry();
+// const count = 50;
+// const positionsArray = new Float32Array(count * 3 * 3);
+// for (let i = 0; i < count * 3 * 3; i++) {
+//     positionsArray[i] = (Math.random() - 0.5) * 4;
+// }
+// const positionsAttribute = new three.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute('position', positionsAttribute);
 
 const material: three.MeshBasicMaterial = new three.MeshBasicMaterial({
-	color: 0x0000ff,
+	color: debugObject.color,
     wireframe: true,
 });
 const mesh: three.Mesh = new three.Mesh(geometry, material);
 
 scene.add(mesh);
 
+const cubeTweaks = gui.addFolder('Cube Tweaks');
+// cubeTweaks.close();
+cubeTweaks.add(mesh.position, 'y').min(-3).max(3).step(0.1).name('elevation');
+cubeTweaks.add(mesh.position, 'x').min(-3).max(3).step(0.1).name('horizontal');
+cubeTweaks.add(mesh, 'visible');
+cubeTweaks.add(material, 'wireframe');
+cubeTweaks.addColor(debugObject, 'color').onChange((colour) => {
+    material.color.set(debugObject.color);
+});
+
+debugObject.spin = () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+}
+
+cubeTweaks.add(debugObject, 'spin');
+
+cubeTweaks.add(debugObject, 'subdivions').min(1).max(20).step(1).onFinishChange(() => {
+    console.log('subvision changed')
+    const newGeometry = new three.BoxGeometry(1, 1, 1, debugObject.subdivions, debugObject.subdivions, debugObject.subdivions);
+    mesh.geometry.dispose();
+    mesh.geometry = newGeometry;
+});
+
+
 // camera
 // fov should be 45-75
 const camera: three.PerspectiveCamera = new three.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-const aspectRatio = sizes.width / sizes.height;
+// const aspectRatio = sizes.width / sizes.height;
 // const camera: three.OrthographicCamera = new three.OrthographicCamera(
 //   -1 * aspectRatio,
 //   1 * aspectRatio,
@@ -83,8 +129,8 @@ const aspectRatio = sizes.width / sizes.height;
 //   100
 // );
 camera.position.z = 3;
-// camera.position.y = 2;
-// camera.position.x = 2;
+camera.position.y = 2;
+camera.position.x = 2;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
